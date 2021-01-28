bb1bcd279e65c540fe80e9bc79c8535422bbd7de
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-28 17:02:57 -0300

    "Comitados arquivos gerais"

---
+++ src/test/java/com/example/sistemapp/SistemappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

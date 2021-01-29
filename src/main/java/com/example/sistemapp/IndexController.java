469e06888903ace567a164be9d00b5f2cc4c20a7
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-29 12:38:48 -0300

    "Incluido novamente arquivo IndexController que foi deletado não sei por quê"

---
+++ src/main/java/com/example/sistemapp/IndexController.java
@@ -0,0 +1,10 @@
+package com.example.sistemapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}

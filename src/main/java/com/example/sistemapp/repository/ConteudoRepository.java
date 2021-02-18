c900caa0f7cd069aae2f76676f424249947f835d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-18 18:28:47 -0300

    "Deletado o conteudo quantPalavras (integer...)"

--- src/main/java/com/example/sistemapp/repository/ConteudoRepository.java
+++ src/main/java/com/example/sistemapp/repository/ConteudoRepository.java
@@ -2,9 +2,12 @@ package com.example.sistemapp.repository;
 
 import com.example.sistemapp.entity.Conteudo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
 
 
-
+@Repository
 public interface ConteudoRepository extends CrudRepository<Conteudo, String> {
     Conteudo findByCodigo(long codigo);
 }
